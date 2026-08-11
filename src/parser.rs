514e9f59694c0fc9b0b9e771f7715cad3e6e7616
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-08-11 16:37:59 +0530

    add: json

--- src/parser.rs
+++ src/parser.rs
@@ -3,6 +3,7 @@ use std::sync::OnceLock;
 
 pub static DATA: OnceLock<Root> = OnceLock::new();
 
+// TODO: switch this from runtime to compile time next
 pub fn get_data() -> &'static Root {
     DATA.get_or_init(|| {
         serde_json::from_str(include_str!("../jsons/wmn-data.json"))
