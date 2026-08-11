2fc0944fdd36355251d2e8c93990e6d957d36e82
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2026-08-11 17:11:02 +0530

    feat: data cleansing

--- src/searcher.rs
+++ src/searcher.rs
@@ -1,8 +1,11 @@
 use anyhow::Result;
 use reqwest::StatusCode;
-use std::time::Duration;
+use std::{borrow::Cow, time::Duration};
 
-use crate::parser::Site;
+use crate::parser::{Headers, Site};
+
+// TODO: set up cli for this later using clap
+static USERNAME: &str = "preetham";
 
 #[derive(Debug)]
 pub struct Response {
@@ -13,5 +16,22 @@ pub struct Response {
 
 // fucking checks the fucking html if fucking found or not
 pub async fn fetch_url(site: &'static Site) -> Result<Response> {
+    // strip naughty characters
+    let username: Cow<'_, str> = match &site.strip_bad_char {
+        Some(naughty) if USERNAME.contains(naughty) => Cow::Owned(USERNAME.replace(naughty, "")),
+        _ => Cow::Borrowed(USERNAME),
+    };
+
+    // meowtube.com/ -> meowtube.com/danny
+    let url = site.uri_check.replace("{account}", USERNAME).trim();
+
+    // req headers cuz fucking browser mf don't wanna think we're botz
+    if let Some(ref s) = site.post_body {
+        // POST
+        let body = s.replace("{account}", &username);
+    } else {
+        // GET
+    }
+
     todo!()
 }
